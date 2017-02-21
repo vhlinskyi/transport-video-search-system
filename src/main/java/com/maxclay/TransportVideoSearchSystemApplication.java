cbaca85211496d457a8a67472dd85e8d16e5cd51
Vlad Glinskiy <glinskiy56@gmail.com>
2017-02-21 13:37:24 +0200

    Initial commit

---
+++ src/main/java/com/maxclay/TransportVideoSearchSystemApplication.java
@@ -0,0 +1,12 @@
+package com.maxclay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportVideoSearchSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportVideoSearchSystemApplication.class, args);
+	}
+}
