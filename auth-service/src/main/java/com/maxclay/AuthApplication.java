409cbab4a3971bcb0c7c6e9e4374214a044b1dda
Vlad Glinskiy <glinskiy56@gmail.com>
2017-03-04 16:30:09 +0200

    Project's hierarchy changed

--- auth-service/src/main/java/com/maxclay/AuthApplication.java
+++ auth-service/src/main/java/com/maxclay/AuthApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TransportVideoSearchSystemApplication {
+public class AuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TransportVideoSearchSystemApplication.class, args);
+		SpringApplication.run(AuthApplication.class, args);
 	}
 }
