fbc1413c81f9dff3df845a7d1541e114a7d0cad3
Vlad Glinskiy <glinskiy56@gmail.com>
2017-04-08 21:02:11 +0300

    Number plate service changed to fetch configuration from config service

--- number-plate-service/src/main/java/com/maxclay/NumberPlateServiceApplication.java
+++ number-plate-service/src/main/java/com/maxclay/NumberPlateServiceApplication.java
@@ -2,12 +2,14 @@ package com.maxclay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NumberPlateServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NumberPlateServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NumberPlateServiceApplication.class, args);
+    }
 
 }
