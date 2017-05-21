19e03e1a1fda22a5e3653fce00bff040aa5c31e6
maxclay <glinskiy56@gmail.com>
2017-05-21 23:19:03 +0300

    Web UI service added. Landing page created.

---
+++ web-ui-service/src/main/java/com/maxclay/WebUIServiceApplication.java
@@ -0,0 +1,16 @@
+package com.maxclay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vlad Glinskiy
+ */
+@SpringBootApplication
+public class WebUIServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebUIServiceApplication.class, args);
+    }
+
+}
